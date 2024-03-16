64de05e02e7a0d047cdaa38e366cd4b1d511afaa
hassanR01 <hassanrageh.236@gmail.com>
2024-03-16 05:41:49 +0200

    again

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -219,7 +219,7 @@ export default function Basket() {
                     <div className="quantity flex flex-row items-center justify-center">
                       <h3 className='font-medium text-xl mx-4'>{item.quantity}</h3>
                     </div>
-                    <div className="price flex">
+                    <div className="price flex items-center justify-center">
                       <h3 className='mr-10 text-2xl font-bold text-green-300'>{item.itemInfo.price * item.quantity} EGP</h3>
                       <svg onClick={() => removeItem(index)} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 sm:mr-4 text-red-400 cursor-pointer"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" /></svg>
                     </div>
