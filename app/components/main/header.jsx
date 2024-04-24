83f400672e04f96290f36ff9e4f7c3aa29c01d6b
hassanR01 <hassanrageh.236@gmail.com>
2024-04-24 19:05:33 +0200

    Add Warta Theme

--- app/components/main/header.jsx
+++ app/components/main/header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
                 WebkitBackgroundSize: "cover",
             }}>
 
-            <div className="nav flex items-center justify-between w-full">
+            <div className="nav flex items-center justify-between w-full mx-2">
                 <Link className='relative' href={'/cart'} >{cart.length > 0 && (<h5 className='absolute right-0 top-0 w-5 h-5 bg-red-500 text-bgColor flex items-center justify-center text-center rounded-full font-bold text-lg'>{cart.length}</h5>)}<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-14 h-12"><path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" /></svg></Link>
 
                 {status === 'authenticated' ? <Link className='rounded-full overflow-hidden border-2 flex items-start justify-center text-xl font-semibold' href={{
@@ -39,9 +39,9 @@ export default function Header() {
             </div>
 
             <div className="image absolute -bottom-32 left-2/4 -translate-x-1/2 flex flex-col items-center justify-center text-center w-full">
-                <Image src={logo} width={120} height={120} alt='Lagham logo' className='bg-gray-950 p-4 rounded-full border-gray-50 border-4 mb-3' />
-                <h4 className='title text-textColor text-2xl mb-2 font-bold '>Lagham Restaurant</h4>
-                <h6 className='text text-sm text-gray-400 font-semibold'>Semoha, Elshahed Eltaiar - B/R2 | 0123-456-7899</h6>
+                <Image src={logo} width={120} height={120} alt='Lagham logo' className='bg-gray-950 rounded-full border-gray-50 border-4 mb-3' />
+                <h4 className='title text-textColor text-2xl mb-2 font-bold '>Warta Restaurant</h4>
+                <h6 className='text text-sm text-gray-400 font-semibold'>Semoha, Elshahed Eltaiar - B/R2 | 0155-626-6697</h6>
             </div>
         </header>
     )
